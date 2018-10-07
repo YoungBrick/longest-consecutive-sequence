18a52bc5780eb3254e35d1b7f486ff99b53654e3
YoungBrick <tidorka@gmail.com>
2018-10-07 15:34:02 +0300

    solved longest-consecutive-sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let set = new Set(array);
+  let max = 0;
+
+  set.forEach(v => {
+    let left = v - 1;
+    let right = v + 1;
+    let current = 1;
+    
+    while(set.has(left)) {
+      set.delete(left--);
+      current++;
+    }
+
+    while(set.has(right)) {
+      set.delete(right++);
+      current++;
+    }
+    if(current > max) max = current;
+
+  })
+ return max;
 }
